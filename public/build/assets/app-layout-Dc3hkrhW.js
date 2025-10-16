5b0281f1a4b729021933f9a0b4c956a1861ced9a
jbombeo <jbombeo@gmail.com>
2025-10-16 21:14:23 +0800

    Deps

---
+++ public/build/assets/app-layout-Dc3hkrhW.js
@@ -0,0 +1,36 @@
+import{j as e,L as d}from"./app-pOO083-e.js";import{S as l,a as h,A as y,b as p,F as r,N as f,c as m,d as x,e as k,f as b,g}from"./app-sidebar-header-COL4hiO2.js";import{d as n}from"./index-DwhOcVGB.js";import{H as u,S as v}from"./settings-BYB2_0H7.js";import{c as i}from"./createLucideIcon-DoMVaiJs.js";import{F as s}from"./file-text-pZ84JjBm.js";/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const M=[["path",{d:"M8 2v4",key:"1cmpym"}],["path",{d:"M16 2v4",key:"4m81vk"}],["rect",{width:"18",height:"18",x:"3",y:"4",rx:"2",key:"1hopcy"}],["path",{d:"M3 10h18",key:"8toen8"}],["path",{d:"M8 14h.01",key:"6423bh"}],["path",{d:"M12 14h.01",key:"1etili"}],["path",{d:"M16 14h.01",key:"1gbofw"}],["path",{d:"M8 18h.01",key:"lrp35t"}],["path",{d:"M12 18h.01",key:"mhygvu"}],["path",{d:"M16 18h.01",key:"kzsmim"}]],C=i("CalendarDays",M);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const j=[["rect",{width:"8",height:"4",x:"8",y:"2",rx:"1",ry:"1",key:"tgr4d6"}],["path",{d:"M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2",key:"116196"}],["path",{d:"M12 11h4",key:"1jrz19"}],["path",{d:"M12 16h4",key:"n85exb"}],["path",{d:"M8 11h.01",key:"1dfujw"}],["path",{d:"M8 16h.01",key:"18s6g9"}]],c=i("ClipboardList",j);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const N=[["rect",{width:"20",height:"14",x:"2",y:"5",rx:"2",key:"ynyp8z"}],["line",{x1:"2",x2:"22",y1:"10",y2:"10",key:"1b3vmo"}]],S=i("CreditCard",N);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const _=[["path",{d:"M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0",key:"1r0f0z"}],["circle",{cx:"12",cy:"10",r:"3",key:"ilqhr7"}]],R=i("MapPin",_);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const w=[["path",{d:"M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2",key:"1yyitq"}],["circle",{cx:"9",cy:"7",r:"4",key:"nufk8"}],["polyline",{points:"16 11 18 13 22 9",key:"1pwet4"}]],A=i("UserCheck",w);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const D=[["path",{d:"M18 21a8 8 0 0 0-16 0",key:"3ypg7q"}],["circle",{cx:"10",cy:"8",r:"5",key:"o932ke"}],["path",{d:"M22 20c0-3.37-2-6.5-4-8a5 5 0 0 0-.45-8.3",key:"10s06x"}]],q=i("UsersRound",D);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const H=[["path",{d:"M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2",key:"1yyitq"}],["circle",{cx:"9",cy:"7",r:"4",key:"nufk8"}],["path",{d:"M22 21v-2a4 4 0 0 0-3-3.87",key:"kshegd"}],["path",{d:"M16 3.13a4 4 0 0 1 0 7.75",key:"1da9ce"}]],L=i("Users",H),U=[{label:"Dashboard",items:[{title:"Dashboard",href:n().url,icon:u}]},{label:"Management",items:[{title:"Barangay Officials",href:"/officials",icon:L},{title:"Residents Record",href:"/residentregistereds",icon:A},{title:"Household Record",href:"/households",icon:q},{title:"Zone",href:"/zones",icon:R}]},{label:"Documents",items:[{title:"Request Document",href:"/documentrequests",icon:r},{title:"Certificates",icon:s,isDropdown:!0,children:[{title:"Barangay Clearance",href:"/barangay-clearances"},{title:"Certificate of Indigency",href:"/certificate-indigency"},{title:"Certificate of Good Moral",href:"/certificate-goodmoral"},{title:"Certificate of Residency",href:"/certificate-residency"}]},{title:"Blotter Records",href:"/blotters",icon:c}]},{label:"Sanguniang Kabataan",items:[{title:"Request Document",href:"/documentrequests",icon:r},{title:"Certificates",icon:s,isDropdown:!0,children:[{title:"Barangay Clearance",href:"/barangay-clearances"},{title:"Certificate of Indigency",href:"/certificate-indigency"},{title:"Certificate of Good Moral",href:"/certificate-goodmoral"},{title:"Certificate of Residency",href:"/certificate-residency"}]},{title:"Blotter Records",href:"/blotters",icon:c}]},{label:"Finance & Services",items:[{title:"Reports",href:"/brgyrevenue",icon:S},{title:"Settings",href:"/services",icon:v}]},{label:"Events",items:[{title:"Activity/Event",href:"/activities",icon:C}]}];function F(){return e.jsxs(l,{collapsible:"icon",variant:"inset",className:"bg-green-600 text-dark",children:[e.jsx(h,{className:"bg-green-600 flex flex-col items-center py-6",children:e.jsx(d,{href:String(n()),prefetch:!0,children:e.jsx(y,{})})}),e.jsx(p,{className:"space-y-2",children:U.map(t=>e.jsxs("div",{children:[e.jsx("p",{className:"px-4 py-1 text-xs font-semibold uppercase text-green-800 tracking-wide",children:t.label}),e.jsx("div",{className:"pl-2 space-y-1",children:e.jsx(f,{items:t.items})})]},t.label))}),e.jsx(m,{children:e.jsx(x,{})})]})}function $({children:t,breadcrumbs:a=[]}){return e.jsxs(k,{variant:"sidebar",children:[e.jsx(F,{}),e.jsxs(b,{variant:"sidebar",className:"overflow-x-hidden",children:[e.jsx(g,{breadcrumbs:a}),t]})]})}const K=({children:t,breadcrumbs:a,...o})=>e.jsx($,{breadcrumbs:a,...o,children:t});export{K as A};
